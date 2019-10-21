1b796dae7aa26da9a50c58dc8ff093beb473b164
唐志红 <tangzhihong@xiao100.com>
2019-10-21 15:58:59 +0800

    'modified'

--- src/test/java/com/example/arithmeticPractice/Problem7.java
+++ src/test/java/com/example/arithmeticPractice/Problem7.java
@@ -22,15 +22,21 @@ public class Problem7 {
         输入: 120
         输出: 21
         注意:
-        假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−231,  231 − 1]。请根据这个假设，如果反转后整数溢出那么就返回 0
+        假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−2^31,  2^31 − 1]。请根据这个假设，如果反转后整数溢出那么就返回 0
          */
     @Test
     public void test(){
-        reverse(-100);
+        reverse(-123456);
+        reverse(-123406);
+        System.out.println(reverse(1534236469));
     }
 
     public int reverse(int x) {
-        String s = String.valueOf(x);
-        return 0;
+        long res = 0;
+        for (; 0 != x;) {
+            res = res * 10 + x%10;
+            x = x/10;
+        }
+        return res < Integer.MIN_VALUE || res > Integer.MAX_VALUE? 0: (int) res;
     }
 }
